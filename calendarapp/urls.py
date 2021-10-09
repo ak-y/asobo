ae497b0e981be46997cc8d79781cacce15da3deb
Marimei <meiwan1014@outlook.jp>
2021-10-09 16:23:14 +0900

    runserveするためにhoge1を編集

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('request', views.request, name='request'),
     path('signout', views.signout, name='signout'),
     path('requester_main', views.requester_main, name='requester_main'),
+
     path('authorize', views.authorize, name='authorize'),
     path('oauth2callback', views.oauth2callback, name='oauth2callback'),
-    path('index', views.index, name='index'),
 ]
