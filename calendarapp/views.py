035b1b52a3891646ff7b5237e2bf7c9821e06846
ak-y <akira.yui305@gmail.com>
2021-10-15 21:01:25 +0900

    views修正

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -35,7 +35,7 @@ def register(request):
             User.objects.create_user(username, '', password)
             user = authenticate(request, username=username, password=password)
             login(request, user)
-            return redirect('authorize', temp='False')
+            return redirect('authorize')
         except IntegrityError:
             return render(request, 'calendarapp/register.html', {
                 'error': 'このユーザーは既に登録されています'
@@ -82,13 +82,7 @@ def main(request):
                 },
             }
             credentials_dict = json.loads(Calendar.objects.get(user=user).credentials)
-            credentials = google.oauth2.credentials.Credentials(
-                token=credentials_dict["token"],
-                refresh_token=credentials_dict["refresh_token"],
-                token_uri=credentials_dict["token_uri"],
-                client_id=credentials_dict["client_id"],
-                client_secret=credentials_dict["client_secret"],
-                scopes=credentials_dict["scopes"])
+            credentials = get_credentials(credentials_dict)
             service = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
             service.events().insert(calendarId='primary', body=body).execute()
 
@@ -102,27 +96,12 @@ def main(request):
 
         return redirect('main')
     else:
+        # 現在日時と30日後の日時をISOフォーマットで取得
+        dt_now_iso, dt_30d_later_iso = get_datetime()
+
+        # Googleカレンダーから予定を取ってくる
         credentials_dict = json.loads(Calendar.objects.get(user=user).credentials)
-        credentials = google.oauth2.credentials.Credentials(
-            token=credentials_dict["token"],
-            refresh_token=credentials_dict["refresh_token"],
-            token_uri=credentials_dict["token_uri"],
-            client_id=credentials_dict["client_id"],
-            client_secret=credentials_dict["client_secret"],
-            scopes=credentials_dict["scopes"])
-
-        # build service and execute
-        service = googleapiclient.discovery.build(
-            API_SERVICE_NAME, API_VERSION, credentials=credentials)
-
-        # カレンダーのリスト取得
-        calendar_id_list = get_calendar_id_list(service)
-
-        # 現在日時と90日後の日時をISOフォーマットで取得
-        dt_now_iso, dt_90d_later_iso = get_datetime()
-
-        # calendar_id_listに追加したそれぞれのカレンダーからイベントを取得
-        event_list = get_event_list(calendar_id_list, service, dt_now_iso, dt_90d_later_iso)
+        event_list = build_service_get_event_list(credentials_dict, dt_now_iso, dt_30d_later_iso)
 
         # リクエスト一覧をデータベースから取得
         requests = Request.objects.filter(user=user, is_accepted=None)
@@ -195,31 +174,19 @@ def requester_main(request, user_id):
     else:
         request.session['user_id'] = user.id
         if 'credentials' not in request.session:  # requesterのカレンダー認証
-            return redirect('authorize', temp='True')
+            request.session['temp'] = 'temp'
+            return redirect('authorize')
+
+        # 時間取得
+        dt_now_iso, dt_30d_later_iso = get_datetime()
 
         # 以下でrequesterの予定を取ってくる
         requester_credentials_dict = request.session['credentials']
-        requester_credentials = get_credentials(requester_credentials_dict)
-
-        requester_service = googleapiclient.discovery.build(
-            API_SERVICE_NAME, API_VERSION, credentials=requester_credentials)
-
-        requester_calendar_id_list = get_calendar_id_list(requester_service)
-
-        dt_now_iso, dt_90d_later_iso = get_datetime()
-
-        requester_event_list = get_event_list(requester_calendar_id_list, requester_service, dt_now_iso, dt_90d_later_iso)
+        requester_event_list = build_service_get_event_list(requester_credentials_dict, dt_now_iso, dt_30d_later_iso)
 
         # 以下でadminの予定を取ってくる
         credentials_dict = json.loads(Calendar.objects.get(user=user).credentials)
-        credentials = get_credentials(credentials_dict)
-
-        service = googleapiclient.discovery.build(
-            API_SERVICE_NAME, API_VERSION, credentials=credentials)
-
-        calendar_id_list = get_calendar_id_list(service)
-
-        event_list = get_event_list(calendar_id_list, service, dt_now_iso, dt_90d_later_iso)
+        event_list = build_service_get_event_list(credentials_dict, dt_now_iso, dt_30d_later_iso)
 
         for event in event_list:
             if event['calendar_id'] != 'ja.japanese#holiday@group.v.calendar.google.com':
@@ -273,10 +240,11 @@ def get_credentials(credentials_dict):
 def get_datetime():
     dt_now = datetime.datetime.now()
     dt_now_iso = dt_now.isoformat()[:19] + 'Z'
-    dt_90d_later_iso = (dt_now + datetime.timedelta(days=90)).isoformat()[:19] + 'Z'
-    return (dt_now_iso, dt_90d_later_iso)
+    dt_30d_later_iso = (dt_now + datetime.timedelta(days=30)).isoformat()[:19] + 'Z'
+    return (dt_now_iso, dt_30d_later_iso)
 
 
+# カレンダーのリスト取得
 def get_calendar_id_list(service):
     calendar_id_list = list()
     page_token = None
@@ -291,13 +259,14 @@ def get_calendar_id_list(service):
     return calendar_id_list
 
 
+# calendar_id_listに追加したそれぞれのカレンダーからイベントを取得
 # イベントの「タイトル(title)、開始日時(start)、終了日時(end)」の情報を辞書形式で持ったevent_infoを作成し、それをevent_listに追加
-def get_event_list(calendar_id_list, service, dt_now_iso, dt_90d_later_iso):
+def get_event_list(calendar_id_list, service, dt_now_iso, dt_30d_later_iso):
     event_list = list()
     for calendar_id in calendar_id_list:
         page_token = None
         while True:
-            events = service.events().list(calendarId=calendar_id, pageToken=page_token, timeMin=dt_now_iso, timeMax=dt_90d_later_iso).execute()
+            events = service.events().list(calendarId=calendar_id, pageToken=page_token, timeMin=dt_now_iso, timeMax=dt_30d_later_iso).execute()
             for event in events['items']:
                 event_info = dict()
                 event_info['calendar_id'] = calendar_id
@@ -316,16 +285,21 @@ def get_event_list(calendar_id_list, service, dt_now_iso, dt_90d_later_iso):
     return event_list
 
 
-def authorize(request, temp):
+def build_service_get_event_list(credentials_dict, dt_now_iso, dt_30d_later_iso):
+    credentials = get_credentials(credentials_dict)
+    service = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials) # build service
+    calendar_id_list = get_calendar_id_list(service)
+    event_list = get_event_list(calendar_id_list,service, dt_now_iso, dt_30d_later_iso)
+    return event_list
+
+
+def authorize(request):
     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE, scopes=SCOPES)
 
     # url when authorization done
-    if temp == 'True':
-        flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback/True'
-    else:
-        flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback/False'
+    flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback'
 
     # authorization-url
     # Enable offline access so that you can refresh an access token without re-prompting the user for permission.
@@ -334,20 +308,17 @@ def authorize(request, temp):
     # Store the state so the callback can verify the auth server response.
     request.session['state'] = state
 
-    return redirect(authorization_url, temp=temp)
+    return redirect(authorization_url)
 
 
-def oauth2callback(request, temp):
+def oauth2callback(request):
     # Specify the state when creating the flow in the callback so that it can
     # verified in the authorization server response.
     state = request.session['state']
 
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    if temp == 'True':
-        flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback/True'
-    else:
-        flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback/False'
+    flow.redirect_uri = 'http://127.0.0.1:8000/oauth2callback'
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
     authorization_response = request.build_absolute_uri()
@@ -355,7 +326,7 @@ def oauth2callback(request, temp):
 
     credentials = flow.credentials
     credentials = credentials_to_dict(credentials)
-    if temp == 'True':  # Store credentials in the session.
+    if 'temp' in request.session:  # Store credentials in the session.
         request.session['credentials'] = credentials
         return redirect('requester_main', user_id=request.session['user_id'])
     else:  # Store credentials in the database.
